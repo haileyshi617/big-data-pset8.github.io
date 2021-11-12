39a2b4f2f0895d5e3d39f8237d50975623e5387d
HuiwenShi <huiwenshi@outlook.com>
2021-11-12 01:07:33 -0500

    add animation

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const svg = d3
   .append('g')
   .attr('transform', `translate(${MARGIN.LEFT}, ${MARGIN.TOP})`);
 
-// Draw
+// Parse data
 d3.csv(dataFile).then((data) => {
   // Sort data
   const dataSorted = data.sort((a, b) =>
@@ -24,7 +24,6 @@ d3.csv(dataFile).then((data) => {
 
   // Scales
   const max = d3.max(dataSorted, (d) => Number(d.total_count));
-
   const x = d3.scaleLinear().domain([0, max]).range([0, WIDTH]);
 
   const y = d3
@@ -34,10 +33,15 @@ d3.csv(dataFile).then((data) => {
     .padding(0.15);
 
   const xAxisCall = d3.axisTop(x).ticks(6);
-  svg.append('g').transition().call(xAxisCall);
+  svg.append('g').transition().duration(1000).call(xAxisCall);
 
   const yAxisCall = d3.axisLeft(y);
-  svg.append('g').transition().attr('id', 'y-axis').call(yAxisCall);
+  svg
+    .append('g')
+    .transition()
+    .duration(1000)
+    .attr('id', 'y-axis')
+    .call(yAxisCall);
 
   // Mouse Event
   const mouseover = function (event, d) {
@@ -61,11 +65,20 @@ d3.csv(dataFile).then((data) => {
     .append('rect')
     .on('mouseover', mouseover)
     .on('mouseout', mouseout)
-    .transition()
     .attr('x', 0)
     .attr('y', (d) => y(d.Name))
-    .attr('width', (d) => x(d.total_count))
+
     .attr('height', y.bandwidth)
     .attr('fill', 'teal')
-    .style('opacity', 0.8);
+    .style('opacity', 0.5)
+
+    .transition()
+    .duration(1000)
+    .attr('width', (d) => x(d.total_count))
+    .attr('fill', 'teal')
+    .ease(d3.easeQuadIn)
+    .style('opacity', 0.8)
+    .delay(function (d, i) {
+      return i * 40;
+    });
 });
